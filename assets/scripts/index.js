6402345eb14b11a221d6117abb5fd60a22cf44aa
Vineet-Singh-Chauhan <94364597+Vineet-Singh-Chauhan@users.noreply.github.com>
2022-05-20 00:27:00 +0530

    frame done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,6 @@
 const navbar = document.querySelector(".navbar");
 window.onscroll = () =>{
-    if (window.pageYOffset >=(navbar.offsetTop+10) ) {
+    if (window.pageYOffset >=(navbar.offsetTop) ) {
         console.log("hello i am at top")
         navbar.classList.add("sticky-nav");
         navbar.classList.remove("default-nav");
